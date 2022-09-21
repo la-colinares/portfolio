bb0ea5213a049c0f3146968157f6a25fa47b75b4
LA Colinares <dev.lacolinares@gmail.com>
2022-09-22 00:37:55 +0800

    develop contact form UI/UX

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './app/scss/style.scss';
 import Hero from './app/components/hero'
 import Projects from './app/components/projects'
 import Skills from './app/components/skills'
+import Contact from './app/components/contact'
 import Footer from './app/components/footer'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Hero/>
       <Projects/>
       <Skills/>
+      <Contact/>
       <Footer/>
     </div>
   )
---
+++ src/app/components/contact/index.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast, TypeOptions } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function index() {
+    const form = useRef<HTMLFormElement>(null)
+    const [buttonDisabled, setButtonDisabled] = useState(false)
+
+    const notify = (msg: string, toastType: TypeOptions) => {
+        toast(msg, {
+            position: "top-right",
+            autoClose: 5000,
+            theme: "dark",
+            type: toastType,
+            hideProgressBar: true,
+            pauseOnFocusLoss: false,
+            
+        })
+    }
+
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        setButtonDisabled(true)
+
+        notify("Sending message...", "info")
+
+        emailjs.sendForm('service_nqh0x1q', 'template_ywelxt9', form.current!!, 'zegVl8JNoU3UWhyoc')
+        .then(() => {
+            notify("Message Sent! Thank you!", "success")
+            setButtonDisabled(false)
+            form.current!!.reset()
+
+        }, () => {
+            notify("Error occured while sending email message. Please try again.", "error")
+        })
+    }
+
+    return (
+        <section className="container">
+            <h1 className="title">Contact Me</h1>
+            <form ref={form} onSubmit={sendEmail} className="contact-form row">
+                <fieldset>
+                    <input placeholder="Full Name:" type="text" name="fullName" tabIndex={1} required autoFocus/>
+                </fieldset>
+                <fieldset>
+                    <input placeholder="Email Address:" type="email" name="email" tabIndex={2} required/>
+                </fieldset>
+                <fieldset>
+                    <textarea placeholder="Message:" name="message" rows={8} tabIndex={3} required></textarea>
+                </fieldset>
+                <fieldset>
+                    <button name="submit" type="submit" disabled={buttonDisabled}>Send</button>
+                </fieldset>
+            </form>
+            <ToastContainer newestOnTop={true}/>
+        </section>
+    )
+}
