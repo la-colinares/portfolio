87a92e89fd7b85729c949808a7d9f4bc6afc51ef
LA Colinares <dev.lacolinares@gmail.com>
2022-09-21 18:58:16 +0800

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './app/scss/style.scss';
 import Hero from './app/components/hero'
 import Projects from './app/components/projects'
 import Skills from './app/components/skills'
+import Footer from './app/components/footer'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Hero/>
       <Projects/>
       <Skills/>
+      <Footer/>
     </div>
   )
 }
---
+++ src/app/components/footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function index() {
+
+    return (
+        <section className="container">
+            <div className="footer-content">
+                <h5 className="footer-text">&#169; 2022 All rights reserved</h5>
+            </div>
+        </section>
+    )
+}
