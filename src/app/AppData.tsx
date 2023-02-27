509a8e4f6283cb4eb82a902b3912b4baa7cd6d9c
LA Colinares <dev.lacolinares@gmail.com>
2023-02-27 10:52:02 +0800

    added android chat gpt project

--- src/app/AppData.tsx
+++ src/app/AppData.tsx
@@ -6,6 +6,7 @@ import bitddMockup from './assets/projects/bitdd.png';
 import rageMockup from './assets/projects/rage_music.png';
 import jetpicMockup from './assets/projects/jetpic.png';
 import learnProgrammingMockup from './assets/projects/learn_programming.png';
+import androidChatGptMockup from './assets/projects/android_chat_gpt.png';
 
 export class HeroData {
     public static readonly profile = profile
@@ -26,6 +27,7 @@ export class ProjectsData {
     public static readonly projects_title = 'Projects'
 
     public static readonly projects = [
+        { image: androidChatGptMockup, title: 'Android Chat GPT', link: 'https://github.com/la-colinares/AndroidChatGPT'},
         { image: learnProgrammingMockup, title: 'Learn Programming', link: 'https://play.google.com/store/apps/details?id=com.lacolinares.learnpro'},
         { image: digidrawMockup, title: 'DigiDraw', link: 'https://github.com/la-colinares/DigiDraw'},
         { image: bitddMockup, title: 'BITDD: Bitcoin Crypto Exchange', link: 'https://play.google.com/store/apps/details?id=com.vestbag.bitdd'},
